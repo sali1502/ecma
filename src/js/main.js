d6dd5c6eff44b26ff103d082866abfee2793c17b
sali1502 <sali1502@student.miun.se>
2024-02-28 18:16:18 +0100

    Test branch - utskrift av länder, namn och population.

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,42 @@
 "use strict";
 
+// test med utskrift av länder
+
+const url = "https://restcountries.com/v3.1/all?fields=name,population";
+
+
+
+
+async function init() {
+    try {
+        const response = await fetch (url);
+        const countries = await response.json();
+
+       displayCountries(countries);
+
+    } catch {
+
+        document.getElementById("error").innerHTML = "<p>Något gick fel, prova igen senare!</p>";
+
+    }
+
+}
+
+init();
+
+function displayCountries(countries) {
+    const countriesEl = document.getElementById("countries-list");
+
+    countries.forEach((country) => {
+        countriesEl.innerHTML +=
+        `<tr>
+            <td>${country.name.common}</td>
+            <td>${country.population}</td>
+        </tr>`;
+    });  
+
+}
+
+
 
 
