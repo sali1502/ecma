49f093ed584949e8f1c42a77ea34f2050b5f86e9
sali1502 <sali1502@student.miun.se>
2024-02-28 19:17:47 +0100

    Övning med sortering och filtrering

--- src/js/main.js
+++ src/js/main.js
@@ -4,13 +4,26 @@
 
 const url = "https://restcountries.com/v3.1/all?fields=name,population";
 
-
-
-
 async function init() {
+
+    // fetch-anrop
+
     try {
         const response = await fetch (url);
         const countries = await response.json();
+
+
+        // Sortera data omvänt med högsta talet först - .reverse().
+        //countries.sort((a, b) => a.population - b.population).reverse();
+
+        // Sortera data utifrån namn omvänt, Ö först- .reverse().
+        //countries.sort((a, b) => (a.name.common > b.name.common) ? 1 : -1).reverse();
+
+        //Filtrera data
+        //let filteredCountries = countries.filter((country) => {
+            //return country.name.common.toLowerCase().includes("an");
+        //});
+
 
        displayCountries(countries);
 
@@ -23,6 +36,9 @@ async function init() {
 }
 
 init();
+
+
+// Utskrift till DOM
 
 function displayCountries(countries) {
     const countriesEl = document.getElementById("countries-list");
