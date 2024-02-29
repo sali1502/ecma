64444d9995732e93c06943136090fb6d34632605
sali1502 <sali1502@student.miun.se>
2024-02-29 10:04:56 +0100

    Utskrift av kurser

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,14 @@
 "use strict";
 
-// test med utskrift av länder
-
-const url = "https://restcountries.com/v3.1/all?fields=name,population";
+const url = "https://webbutveckling.miun.se/files/ramschema_ht23.json";
 
 async function init() {
 
     // fetch-anrop
 
     try {
-        const response = await fetch (url);
-        const countries = await response.json();
+        const response = await fetch(url);
+        const courses = await response.json();
 
 
         // Sortera data omvänt med högsta talet först - .reverse().
@@ -20,12 +18,12 @@ async function init() {
         //countries.sort((a, b) => (a.name.common > b.name.common) ? 1 : -1).reverse();
 
         //Filtrera data
-        let filteredCountries = countries.filter((country) => {
-            return country.name.common.toLowerCase().includes("an");
-        });
+        //let filteredCountries = countries.filter((country) => {
+            //return country.name.common.toLowerCase().includes("an");
+        //});
 
 
-       displayCountries(countries);
+        displayCourses(courses);
 
     } catch {
 
@@ -40,16 +38,17 @@ init();
 
 // Utskrift till DOM
 
-function displayCountries(countries) {
-    const countriesEl = document.getElementById("countries-list");
+function displayCourses(courses) {
+    const coursesEl = document.getElementById("course-list");
 
-    countries.forEach((country) => {
-        countriesEl.innerHTML +=
-        `<tr>
-            <td>${country.name.common}</td>
-            <td>${country.population}</td>
+    courses.forEach((course) => {
+        coursesEl.innerHTML +=
+            `<tr>
+            <td>${course.code}</td>
+            <td>${course.coursename}</td>
+            <td>${course.progression}</td>
         </tr>`;
-    });  
+    });
 
 }
 
