fdf5e26ce8e9edbd72bc134cd7f55116cd93d4b1
sali1502 <sali1502@student.miun.se>
2024-03-04 14:20:09 +0100

    Fungerande eventlyssnare sort/filter.

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,75 @@
 "use strict";
 
-
+// Variabler
 const url = "https://webbutveckling.miun.se/files/ramschema_ht23.json";
+const searchBarEl = document.getElementById('searchBar');
+const codeEl = document.getElementById('code');
+const courseNameEl = document.getElementById('courseName');
+const progressionEl = document.getElementById('progression');
+
+let courses = [];
+
+window.onload = init;
 
 async function init() {
 
-    // fetch-anrop
-
+    // Fetch-anrop
     try {
         const response = await fetch(url);
-        const courses = await response.json();
+        courses = await response.json();
 
-    //Sorterar data från lägst till högst. .reverse(); gör det omvända.
+        // Händelsehanterare sorterar kurskod vid klick på rubrik
+        codeEl.addEventListener('click', (e) => {
+            let sortedCode = courses.sort((a, b) => (a.code > b.code) ? 1 : -1);
 
-    courses.sort((a, b) => (a.code > b.code) ? 1 : -1);
+            displayCourses(sortedCode);
+        });
 
-    courses.sort((a, b) => (a.coursename > b.coursename) ? 1 : -1);
+        // Händelsehanterare sorterar kurskod omvänt vid dubbelklick på rubrik
+        codeEl.addEventListener('dblclick', (e) => {
+            let sortedCodeReverse = courses.sort((a, b) => (a.code > b.code) ? 1 : -1).reverse();
 
-    courses.sort((a, b) => (a.progression > b.progression) ? 1 : -1);
+            displayCourses(sortedCodeReverse);
+        });
 
-    //Filtrerar data
-        let filteredCourses = courses.filter((course) => {
-            return course.coursename.includes("webb");
+        // Händelsehanterare sorterar kursnamn vid klick på rubrik
+        courseNameEl.addEventListener('click', (e) => {
+            let sortedCourseName = courses.sort((a, b) => (a.coursename > b.coursename) ? 1 : -1);
+
+            displayCourses(sortedCourseName);
+        });
+
+        // Händelsehanterare sorterar kursnamn omvänt vid dubbelklick på rubrik
+        courseNameEl.addEventListener('dblclick', (e) => {
+            let sortedCourseNameReverse = courses.sort((a, b) => (a.coursename > b.coursename) ? 1 : -1).reverse();
+
+            displayCourses(sortedCourseNameReverse);
+        });
+
+        // Händelsehanterare som sorterar progression vid klick på rubrik
+        progressionEl.addEventListener('click', (e) => {
+            let sortedProgression = courses.sort((a, b) => (a.progression > b.progression) ? 1 : -1);
+
+            displayCourses(sortedProgression);
+        });
+
+        // Händelsehanterare som sorterar progression omvänt vid dubbelklick på rubrik
+        progressionEl.addEventListener('dblclick', (e) => {
+            let sortedProgressionReverse = courses.sort((a, b) => (a.progression > b.progression) ? 1 : -1).reverse();
+
+            displayCourses(sortedProgressionReverse);
+        });
+
+        // Händelsehanterare som filtrerar data vid input i sökruta
+        searchBarEl.addEventListener('keyup', (e) => {
+            const searchString = e.target.value;
+
+            let filteredCourses = courses.filter((course) => {
+                return course.coursename.toLowerCase().includes(searchString);
             });
-        
-        //displayCourses(filteredCourses);
+
+            displayCourses(filteredCourses);
+        });
 
         displayCourses(courses);
 
@@ -36,37 +81,24 @@ async function init() {
 
 }
 
-init()
-
-
 // Utskrift till DOM
+const coursesEl = document.getElementById("courseList");
 
-function displayCourses(courses) {
-    const coursesEl = document.getElementById("course-list");
-
-    courses.forEach((course) => {
-        coursesEl.innerHTML +=
-            `<tr>
+const displayCourses = (courses) => {
+    const htmlString = courses
+        .map((course) => {
+            return `
+        <tr>
             <td>${course.code}</td>
             <td>${course.coursename}</td>
             <td>${course.progression}</td>
-        </tr>`;
-    });
-
-}
-
-
-//Eventlyssnartest
-
-let courseListEl = document.getElementById("course-list");
-courseListEl.addEventListener("click", sort);
-
-function sort(courses) {
-let coursesSort = courses.sort((a, b) => (a.code > b.code) ? 1 : -1);
-return coursesSort;
+        </tr>
+        `;
+        })
+        .join('');
+    coursesEl.innerHTML = htmlString;
 };
 
-displayCourses(coursesSort);
 
 
 
